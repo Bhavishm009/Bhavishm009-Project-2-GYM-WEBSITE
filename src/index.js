4a3fbc321527743c719703f790dc5c03f7d0c94a
Bhavishm009 <bhavishm009@gmail.com>
2023-01-15 23:00:17 +0530

    New Project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import SignUp from '../src/components/home/SignUp'
+import Login from './components/home/Login';
+import { RecoilRoot } from 'recoil';
+import AppPricing from './components/home/pricing';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <RecoilRoot>
+  <Routes>
+    <Route path='/' element={ <App />}></Route>
+    <Route path='/SignUp' element={<SignUp/>}></Route>
+    <Route path='/Login' element={<Login/>}></Route>
+    <Route path='/AppPricing' element={<AppPricing/>}></Route>
+    
+  </Routes>
+  </RecoilRoot>
+  </BrowserRouter>
+    
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
